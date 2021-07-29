78e229652fbf784158f0f8c0b6804f8a506d7539
aryan_mor <aryanmoradi74@gmail.com>
2021-07-29 14:44:52 +0430

    SSR bug fixed

--- src/helper/useWindowSize.js
+++ src/helper/useWindowSize.js
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import debounce from './debounce'
 
 export default function useWindowSize(wait = 2000) {
 
   const [size, setSize] = useState([0, 0])
 
-  useEffect(updateSize, [])
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    updateSize()
     try {
-      window.addEventListener('resize', debounce(function() {
+      window?.addEventListener('resize', debounce(function() {
         updateSize()
       }, wait))
-      return () => window.removeEventListener('resize', updateSize)
-    } catch {
-    }
-  }, [window])
+      return () => window?.removeEventListener('resize', updateSize)
+    } catch {}
+  }, [])
 
 
   function updateSize() {
     try {
-      setSize([window.innerWidth, window.innerHeight])
+      setSize([window?.innerWidth, window?.innerHeight])
     } catch {
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Img from './components/img/Img'
 export default Img;
 
-export { default as getImageSize } from './helper/getImageSize'
+export { default as useWindowSize } from './helper/useWindowSize';
+
 
 import './styles.module.css'
